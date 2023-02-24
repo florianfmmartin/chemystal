52c53095e13db162b1d3b019c2fd73ffae44e2c1
florianfmmartin <florian.fm.martin@gmail.com>
2023-02-24 14:36:28 -0500

    docs: add documentation to molecules

--- src/chemystal/chemical_machine.cr
+++ src/chemystal/chemical_machine.cr
@@ -1,29 +1,41 @@
 require "./molecule.cr"
 
 module Chemystal
-  alias MoleculeSoup = Hash(Symbol, Deque(Molecule))
+  # A Molecule Soup is a Hash from the molecules name
+  # to a deque of molecules having that same name
+  #
+  # The use of a deque is to have FIFO, such that no old molecules
+  # are left floating around in the soup
+  alias MoleculeSoup = Hash(String, Deque(Molecule))
 
+  # A Chemical Machine has a molecule soup and a reaction collection.
+  # When activated it runs reactions based on the molecules available
+  # in the soup as ressources for those reactions
   class ChemicalMachine
     @molecule_soup = MoleculeSoup.new
 
-    protected def get_molecules_with_name(name : Symbol) : Deque(Molecule)
+    # Gets all molecules with the *name*
+    protected def get_molecules_with_name(name : String) : Deque(Molecule)
       return @molecule_soup.fetch name, Deque(Molecule).new
     end
 
-    protected def has_molecule_with_name?(name : Symbol) : Bool
+    # Checks if at least one molecule with the *name* is in the soup
+    protected def has_molecule_with_name?(name : String) : Bool
       return get_molecules_with_name(name).size > 0
     end
 
-    protected def use_molecule_with_name(name : Symbol) : Molecule | Nil
+    # Consumes one molecule withe the *name*, returning it and removing it from the soup
+    protected def use_molecule_with_name(name : String) : Molecule | Nil
       molecules_with_same_name = @molecule_soup.fetch name, nil
 
       if molecules_with_same_name.nil?
         return nil
       end
 
-      return molecules_with_same_name.pop?
+      return molecules_with_same_name.shift?
     end
 
+    # Adds a molecule to the soup
     def add_molecule(molecule : Molecule)
       molecules_with_same_name = @molecule_soup.fetch molecule.name, Deque(Molecule).new
       molecules_with_same_name.push molecule
--- src/chemystal/molecule.cr
+++ src/chemystal/molecule.cr
@@ -1,10 +1,13 @@
 module Chemystal
-  alias MoleculeValue = Bool | Char | Float64 | Int64 | Nil | String | UInt64
+  # A Molecule can take all these values
+  alias MoleculeValue = Bool | Char | Float64 | Int64 | Nil | String | UInt64 | Hash(MoleculeValue, MoleculeValue)
 
+  # A Molecule is a labeled value, it is similar to a typed message
+  # in message passing paradigms
   struct Molecule
     getter name, value
 
-    def initialize(@name : Symbol, @value : MoleculeValue)
+    def initialize(@name : String, @value : MoleculeValue)
     end
   end
 end
